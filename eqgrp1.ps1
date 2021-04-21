9bb4ce5906e290e0bf5e7550006ea6458354f772
Aleksander Spro <abspro@gmail.com>
2021-04-21 22:02:50 +0200

    Pause between each startup

--- eqgrp1.ps1
+++ eqgrp1.ps1
@@ -16,8 +16,13 @@ if (!$isRunning)
 
 $eqgamePath='C:\Games\Everquest'
 Start-Process -FilePath "eqgame.exe" -WorkingDirectory $eqgamePath -ArgumentList "patchme -h /login:eqretro2"
+Start-Sleep -Seconds 3
 Start-Process -FilePath "eqgame.exe" -WorkingDirectory $eqgamePath -ArgumentList "patchme -h /login:eqretro3"
+Start-Sleep -Seconds 3
 Start-Process -FilePath "eqgame.exe" -WorkingDirectory $eqgamePath -ArgumentList "patchme -h /login:eqretro4"
+Start-Sleep -Seconds 3
 Start-Process -FilePath "eqgame.exe" -WorkingDirectory $eqgamePath -ArgumentList "patchme -h /login:eqretro5"
+Start-Sleep -Seconds 3
 Start-Process -FilePath "eqgame.exe" -WorkingDirectory $eqgamePath -ArgumentList "patchme -h /login:eqretro6"
+Start-Sleep -Seconds 3
 Start-Process -FilePath "eqgame.exe" -WorkingDirectory $eqgamePath -ArgumentList "patchme -h /login:eqretro"
