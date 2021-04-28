0b17a7ba7f164a92cff903032b205827074ec76e
Aleksander Spro <abspro@gmail.com>
2021-04-28 17:55:12 +0200

    Update eqgrp1.bat

--- eqgrp1.bat
+++ eqgrp1.bat
@@ -4,9 +4,9 @@ REM timeout 1
 REM start Macroquest2.exe
 REM timeout 1
 cd /D "C:\Games\Everquest"
-start eqgame.exe patchme -h /login:eqretro2
-start eqgame.exe patchme -h /login:eqretro3
-start eqgame.exe patchme -h /login:eqretro4
-start eqgame.exe patchme -h /login:eqretro5
-start eqgame.exe patchme -h /login:eqretro6
-start eqgame.exe patchme -h /login:eqretro
+start eqgame.exe patchme -h /login:
+start eqgame.exe patchme -h /login:
+start eqgame.exe patchme -h /login:
+start eqgame.exe patchme -h /login:
+start eqgame.exe patchme -h /login:
+start eqgame.exe patchme -h /login:
