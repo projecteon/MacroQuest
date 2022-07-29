89b2a0bf7b34ef03e5c11cf60de6cffa0774c605
Aleksander Spro <abspro@gmail.com>
2022-07-29 16:03:11 +0200

    More sample code

--- lua/examples/uisample.lua
+++ lua/examples/uisample.lua
@@ -63,3 +63,26 @@ local pctMana = isnetbots(raidname, 'PctMana', 0)
 local targetName = isnetbots(raidname, 'Name', 'No Target')
 local Attacking = isnetbots(raidname, "Attacking", false)
 local Casting = isnetbots(raidname, "Casting", false)
+
+
+-- /itemnotify in pack1-10 itemslot2+1 i think
+-- and itemslot would be -22 to get the pack number
+-- so i restack that by setting bagstart and bagend to between 1 and 10
+-- so we first have to convert it to itemslot by adding 22 so the me.inventory can work with it, then when i itemnofity i substract 22 from the bag
+local function restack()
+  print(string.format('\agRestacking bag %d to %d', BagStart, BagEnd))
+  for bag = BagStart,BagEnd do
+      bag = bag+22
+      for slot = 1, mq.TLO.Me.Inventory(bag).Container() do
+          if mq.TLO.Me.Inventory(bag).Item(slot)() then
+              mq.cmdf('/nomodkey /shiftkey /itemnotify in pack%d %d leftmouseup', bag-22, slot)
+              mq.delay(500,function() return mq.TLO.Cursor() ~= nil end)
+              while mq.TLO.Cursor.ID() do
+                  mq.cmd('/autoinventory')
+                  mq.delay(100)
+              end
+          end
+      end
+  end
+  print('\agRestacking done')
+end
